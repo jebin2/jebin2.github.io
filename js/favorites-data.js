25b375b3773091e5de1492ec65cd7c5c35374334
Jebin Einstein <jebineinstein@gmail.com>
2025-12-10 15:12:57 +0530

    fmhy

--- js/favorites-data.js
+++ js/favorites-data.js
@@ -66,7 +66,117 @@ export const favoriteWebsites = [
     {
         title: "The largest collection of free stuff on the internet!",
         url: "https://fmhy.net",
+        emoji: "🎁"
+    },
+    {
+        title: "UNIX MAGIC",
+        url: "https://unixmagic.net/",
+        emoji: "🖥️"
+    },
+    {
+        title: "Float Exposed",
+        url: "https://float.exposed/",
         emoji: "📝"
+    },
+    {
+        title: "Gaultier's Blog",
+        url: "https://gaultier.github.io/blog/",
+        emoji: "📎"
+    },
+    {
+        title: "Explorar Dev",
+        url: "https://explorar.dev/",
+        emoji: "📎"
+    },
+    {
+        title: "Comic Browserling",
+        url: "https://comic.browserling.com/",
+        emoji: "🖥️"
+    },
+    {
+        title: "Azeria Labs",
+        url: "https://azeria-labs.com/",
+        emoji: "⛳"
+    },
+    {
+        title: "SDF Public Access UNIX System",
+        url: "https://sdf.org/",
+        emoji: "🖥️"
+    },
+    {
+        title: "A Soft Murmur",
+        url: "https://asoftmurmur.com/",
+        emoji: "🎧"
+    },
+    {
+        title: "Neal.fun",
+        url: "https://neal.fun/",
+        emoji: "🎉"
+    },
+    {
+        title: "Window Swap",
+        url: "https://www.window-swap.com/",
+        emoji: "🌍"
+    },
+    {
+        title: "Zoomquilt",
+        url: "https://zzz.zoomquilt.org/",
+        emoji: "🌌"
+    },
+    {
+        title: "The Useless Web",
+        url: "https://theuselessweb.com/",
+        emoji: "🌀"
+    },
+    {
+        title: "Calligram",
+        url: "https://paint.toys/calligram/",
+        emoji: "🎨"
+    },
+    {
+        title: "Maze Toys",
+        url: "https://maze.toys/",
+        emoji: "🧩"
+    },
+    {
+        title: "Hacker Typer",
+        url: "https://hackertyper.net/",
+        emoji: "💻"
+    },
+    {
+        title: "Pointer Pointer",
+        url: "https://pointerpointer.com/",
+        emoji: "👉"
+    },
+    {
+        title: "Puginarug",
+        url: "https://puginarug.com/",
+        emoji: "🐶"
+    },
+    {
+        title: "Weavesilk",
+        url: "http://weavesilk.com/",
+        emoji: "🕸  ️"
+    },
+    {
+        title: "Optical Toys",
+        url: "https://optical.toys/",
+        emoji: "👓"
+    },
+    {
+        title: "Jackson Pollock",
+        url: "https://jacksonpollock.org/",
+        emoji: "🎨"
+    },
+    {
+        title: "Drawing Garden",
+        url: "https://drawing.garden/",
+        emoji: "🌸"
+    },
+    {
+        title: "Slow Roads",
+        url: "https://slowroads.io/",
+        emoji: "🛤️"
     }
     // Add more websites here following the same format
 ];
